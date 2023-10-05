41fc9137babe59281722509c85ef6ca16b0475d1
Andrei Dochkin <andrey.dochkin@gmail.com>
2023-10-05 21:15:09 +0400

    build(npm): remove unnecessary pack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import React from 'react';
+// import { useEffect, useState } from "react";
+// import { Route, Navigate, Routes, useNavigate, useLocation } from 'react-router-dom';
 
 function App() {
 	return (
