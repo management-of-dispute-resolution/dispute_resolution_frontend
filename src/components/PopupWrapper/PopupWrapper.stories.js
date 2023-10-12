4e9775e1adef3f1fcaa30fa4ebb6164d66d4e7b7
Sergey Evdokimov <cadsergrf@yandex.ru>
2023-10-12 13:57:41 +0700

    feat: верстка компонентов: 1- обертка для модальных окон  2 - информационный попап

---
+++ src/components/PopupWrapper/PopupWrapper.stories.js
@@ -0,0 +1,12 @@
+import { PopupWrapper } from './PopupWrapper';
+
+export default {
+	title: 'Dispute_Resolution/PopupWrapper',
+	component: PopupWrapper,
+};
+
+export const Visible = {
+	args: {
+		isOpen: true,
+	},
+};
