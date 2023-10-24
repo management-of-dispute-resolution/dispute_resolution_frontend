2eea25f1373c6400d917e606c629b5a315d980c5
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-10-24 03:45:57 +0300

    feat(button.jsx): refactoring, add new type transperent css type active/focus/disabled/hover

--- src/components/ui-kit/Button/Button.stories.js
+++ src/components/ui-kit/Button/Button.stories.js
@@ -8,9 +8,68 @@ export default {
 		backgroundColor: { control: 'color' },
 	},
 };
-export const Large = {
+export const Default = {
 	args: {
-		// primary: true,
+		// size: 'large',
 		label: 'Button',
 	},
 };
+export function Type(args) {
+	return (
+		<div className="button-box">
+			<Button
+				size="medium"
+				label="Создать обращение"
+				color="downy"
+				type="button"
+				{...args}
+			/>
+			<Button
+				size="large"
+				label="Продолжить"
+				color="blueLagoon"
+				type="button"
+				{...args}
+			/>
+			<Button
+				size="large"
+				label="Добавить оппонентов"
+				color="blueLagoon-inverted"
+				type="button"
+				{...args}
+			/>
+			<Button
+				size="small"
+				label="Выход"
+				color="transperent"
+				type="button"
+				before="exit"
+				{...args}
+			/>
+			<Button
+				size="small"
+				label="Сменить пароль"
+				color="transperent"
+				type="button"
+				before="changePassword"
+				{...args}
+			/>
+			<Button
+				size="small"
+				label="Редактировать"
+				color="transperent"
+				type="button"
+				before="edit"
+				{...args}
+			/>
+			<Button
+				size="small"
+				label="Отменить обращение"
+				color="transperent"
+				type="button"
+				before="cancel"
+				{...args}
+			/>
+		</div>
+	);
+}
