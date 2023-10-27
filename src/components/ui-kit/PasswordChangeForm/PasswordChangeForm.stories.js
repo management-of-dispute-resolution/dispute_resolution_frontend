c28986bb90905abae52082a4e2be237082c75231
eilyina <47791195+eilyina@users.noreply.github.com>
2023-10-27 20:47:09 +0500

    Feature/change password form (#31)
    * feat: add edit form
    
    * style: add styles to edit form
    
    * fix: fix rw

---
+++ src/components/ui-kit/PasswordChangeForm/PasswordChangeForm.stories.js
@@ -0,0 +1,13 @@
+import PasswordChangeForm from './PasswordChangeForm';
+
+export default {
+	title: 'Dispute_Resolution/Forms/ChangePassword',
+	component: PasswordChangeForm,
+};
+
+export const PasswordForm = {
+	args: {
+		statusMessage: '',
+		disabled: false,
+	},
+};
