9b7dc3a6c80a85377de81c7b67f1c76418159ac6
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-11-18 05:23:23 +0300

    fix(filelist): hOTFIX
    Исправил пропсы, удалил лишнее, исправил ошибки в devtools

--- src/components/ui-kit/FileList/FileList.jsx
+++ src/components/ui-kit/FileList/FileList.jsx
@@ -7,24 +7,16 @@ function FileList({ files, type }) {
 	return (
 		<div className={`files files_type_${type}`}>
 			{files.map((item) => {
-				// const nameWithFormat = file.split('/').pop();
-				// const format = nameWithFormat.split('.').pop();
-				// const name = nameWithFormat.split('.')[0]; // Извлекаем только название файла
-				// const downloadLink = files;
-
-				// Правка под формат бекенда
-				const nameWithFormat = item.file.split('/').pop();
-				const format = nameWithFormat.split('.').pop();
-				const name = item.filename; 
-				const downloadLink = item.file;
-
+				const name = item.filename;
+				const format = item.file.split('.').pop();
+				const downloadLink = files.file;
 				return (
 					<a
 						className="file"
 						href={downloadLink}
 						target="_blank"
 						rel="noreferrer"
-						key={item}
+						key={item.id}
 					>
 						<img className="file__icon" src={ImageConfig[format]} alt="File" />
 						<p className="file__name">{name}</p>
@@ -35,11 +27,18 @@ function FileList({ files, type }) {
 	);
 }
 FileList.propTypes = {
-	files: PropTypes.arrayOf(PropTypes.string),
-	type: PropTypes.PropTypes.oneOf(['dispute', 'message']),
+	files: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			filename: PropTypes.string.isRequired,
+			file: PropTypes.string.isRequired,
+		})
+	),
+	type: PropTypes.oneOf(['dispute', 'message']),
 };
+
 FileList.defaultProps = {
-	files: [],
+	files: {},
 	type: 'dispute',
 };
 
