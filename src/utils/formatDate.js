dc2edf49497af36823081b066a0d7e34740e010a
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-11-26 00:23:43 +0300

    formatDate ref

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,26 +1,28 @@
-	function formatDate(dateStr, short = false) {
-		const months = [
-			'янв',
-			'фев',
-			'мар',
-			'апр',
-			'май',
-			'июн',
-			'июл',
-			'авг',
-			'сен',
-			'окт',
-			'ноя',
-			'дек',
-		];
+function formatDate(inputDate, short) {
+	const currentDate = new Date();
+	const inputDateTime = new Date(inputDate);
 
-		const d = new Date(dateStr);
-		const day = d.getDate();
-		const month = months[d.getMonth()];
-		const hours = d.getHours();
-		const minutes = `${d.getMinutes()}`.padStart(2, '0');
+	if (!short) {
+		const options = {
+			day: 'numeric',
+			month: 'short',
+			hour: 'numeric',
+			minute: 'numeric',
+		};
 
-		return short ? `${day} ${month}` : `${day} ${month} в ${hours}:${minutes}`;
+		return inputDateTime.toLocaleDateString('ru-RU', options);
+	}
+	const options = {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	};
+
+	if (currentDate.getFullYear() === inputDateTime.getFullYear()) {
+		// Если год совпадает, отображаем без года
+		options.year = undefined;
 	}
 
-    export default formatDate;
+	return inputDateTime.toLocaleDateString('ru-RU', options);
+}
+export default formatDate;
